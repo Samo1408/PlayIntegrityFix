fb0050f173001231b67fd47661172ff5d73f220f
Samo1408 <ab.0ss0mm@gmail.com>
2026-04-23 16:01:53 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,35 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-}
+      alias(libs.plugins.android.application) apply false
+  }
 
-tasks.register("copyZygiskFiles") {
-    val moduleFolder = project.rootDir.resolve("module")
-    val zygiskModule = project.project(":zygisk")
-    val zygiskBuildDir = zygiskModule.layout.buildDirectory
-    val classesJar = zygiskBuildDir.file("intermediates/dex/release/minifyReleaseWithR8/classes.dex")
-    val zygiskSoDir = zygiskBuildDir.file("intermediates/stripped_native_libs/release/stripReleaseDebugSymbols/out/lib")
+  tasks.register("copyZygiskFiles") {
+      val moduleFolder = project.rootDir.resolve("module")
+      val zygiskModule = project.project(":zygisk")
+      val zygiskBuildDir = zygiskModule.layout.buildDirectory
+      val classesJar = zygiskBuildDir.file("intermediates/dex/release/minifyReleaseWithR8/classes.dex")
+      val zygiskSoDir = zygiskBuildDir.file("intermediates/stripped_native_libs/release/stripReleaseDebugSymbols/out/lib")
 
-    inputs.dir(zygiskSoDir)
-    inputs.file(classesJar)
-    outputs.dir(moduleFolder)
+      inputs.dir(zygiskSoDir)
+      inputs.file(classesJar)
+      outputs.dir(moduleFolder)
 
-    doLast {
-        classesJar.get().asFile.copyTo(moduleFolder.resolve("classes.dex"), overwrite = true)
-        moduleFolder.resolve("inject").deleteRecursively()
-        zygiskSoDir.get().asFile.walk()
-            .filter { it.isFile && it.name == "libzygisk.so" }
-            .forEach { soFile ->
-                val abiFolder = soFile.parentFile.name
-                val destination = moduleFolder.resolve("zygisk/$abiFolder.so")
-                soFile.copyTo(destination, overwrite = true)
-            }
-    }
-}
+      doLast {
+          classesJar.get().asFile.copyTo(moduleFolder.resolve("classes.dex"), overwrite = true)
+          moduleFolder.resolve("inject").deleteRecursively()
+          zygiskSoDir.get().asFile.walk()
+              .filter { it.isFile && it.name == "libzygisk.so" }
+              .forEach { soFile ->
+                  val abiFolder = soFile.parentFile.name
+                  val destination = moduleFolder.resolve("zygisk/$abiFolder.so")
+                  soFile.copyTo(destination, overwrite = true)
+              }
+      }
+  }
 
-tasks.register<Zip>("zip") {
-    dependsOn("copyZygiskFiles")
-
-    archiveFileName.set("PlayIntegrityFix.zip")
-    destinationDirectory.set(project.rootDir.resolve("out"))
-
-    from(project.rootDir.resolve("module"))
-}
+  tasks.register<Zip>("zip") {
+      dependsOn("copyZygiskFiles")
+      archiveFileName.set("TeleInject.zip")
+      destinationDirectory.set(project.rootDir.resolve("out"))
+      from(project.rootDir.resolve("module"))
+  }
+  
